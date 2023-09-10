2a7fa7f788b828077865c58a658f224cf0c7d464
Razmik Achikyan <razmikachikyan5@gmail.com>
2023-09-10 21:12:00 +0400

    lesson 1 task text parser

---
+++ lesson_1/parser.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+class CommandParser {
+public:
+    CommandParser(const std::string& input) : input(input) {}
+
+    struct Command {
+        std::string name;
+        std::vector<std::string> arguments;
+    };
+
+    Command parseInput() {
+        Command cmd;
+
+        std::istringstream iss(input);
+        iss >> cmd.name; // Extract the first word as the command
+
+        std::string arg;
+        while (iss >> arg) {
+            cmd.arguments.push_back(arg);
+        }
+
+        return cmd;
+    }
+
+private:
+    std::string input;
+};
+
+int main() {
+    std::string input;
+    std::cout << "Enter a command: ";
+    std::getline(std::cin, input);
+
+    CommandParser parser(input);
+    CommandParser::Command cmd = parser.parseInput();
+
+    // Assuming some predefined commands
+    if (cmd.name == "echo") {
+        for (const std::string& arg : cmd.arguments) {
+            std::cout << arg << " ";
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "Unknown command." << std::endl;
+    }
+
+    return 0;
+}
